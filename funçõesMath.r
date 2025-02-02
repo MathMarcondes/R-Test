f30d7a3354a82c0961da5d5a910c4741bb49b9e4
MathMarcondes <matheus.marcondes@outlook.com>
2025-02-02 10:22:13 -0300

    Operações básicas

---
+++ funçõesMath.r
@@ -0,0 +1,12 @@
+c <- 25
+sqrt(c) #raíz quadrada
+factorial(c)
+y <- -6
+sqrt(y)
+abs(c) #Valor Absoluto
+log(c, base = 2) # -> Logaritmos
+
+z <- log(c, base = 2)
+round(z, digits = 2) #Arredondamento (no caso para duas casas decimais)
+signif(z, digits = 2)  # Números Significativos (Paricido com a função round) Número de digitos que vai aparecer no parâmetro
+
